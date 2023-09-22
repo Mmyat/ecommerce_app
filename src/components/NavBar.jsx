815871d42d9f55569bf217bdb775c021fe626afb
Mmyat <mmyathaha@gmail.com>
2023-09-22 16:08:40 +0630

    responsive design-3

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
     <nav className="w-full flex flex-wrap items-start justify-between fixed top-0 left-0 right-0 bg-gray-300 px-1 py-2 mx-auto mb-3 shadow-md rounded-b-lg">
             <div className="flex items-center mr-auto">
               <SiShopify className="text-2xl" style={{color:"#ff8906"}}/>
-              <h1 className="navbar-brand text-header font-semibold text-xl">Shop</h1>
+              <h1 className="navbar-brand text-header font-semibold text-lg md:text-xl lg:text-2xl">Shop</h1>
             </div>
             <div className="flex items-center">
               <NavLink to={"/"} className="navbar-default" aria-current="page" href="#">
@@ -20,11 +20,11 @@ const NavBar = () => {
             </div>
             <div className="flex items-center bg-white border-3 mx-auto px-2 py-1 rounded">
               <HiSearch className="text-2xl"/>
-              <input className="outline-non bg-transparent" value={search} onChange={(e)=>setSearch(e.target.value)} type="text" placeholder="Search"/>
+              <input className="outline-non w-16 md:w-36 lg:w-52 bg-transparent" value={search} onChange={(e)=>setSearch(e.target.value)} type="text" placeholder="Search"/>
             </div>
             <div className="flex items-center">
               <NavLink to={"/cart"} className="nav-link flex bg-header rounded p-1 items-center gap-3" href="#">
-                <FaShoppingCart className="text-xl text-white"/>
+                <FaShoppingCart className="md:text-lg sm:text-sm text-white"/>
                 <h3 className="text-white">{cart.length}</h3>
               </NavLink>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,22 @@ export const theme = {
       "header" : "#0e172c",
       "info" : "#f9bc60",
       "warn" : "#ff8906"
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     }
   },
 };
